b8f5bddab80836f14034dc967e4bdc0a05959b7a
Reza Amini <rezaaminiroyal@gmail.com>
2021-09-17 22:56:57 +0430

    Make middleware in more flexible

--- src/routes/migrator.php
+++ src/routes/migrator.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get(config('migrator.route'), \Migrator\Http\Livewire\Migration\Read::class)
-    ->name('migrator.read')->middleware(['web', config('migrator.middleware')]);
+    ->name('migrator.read')->middleware(config('migrator.middleware') ? ['web', config('migrator.middleware')] : ['web']);
