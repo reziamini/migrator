e5c5ca3b4381c0f1e280980026f8aee084879d4c
Reza Amini <rezaaminiroyal@gmail.com>
2021-09-20 20:56:28 +0430

    Change the name

--- resources/views/livewire/migration/read.blade.php
+++ resources/views/livewire/migration/read.blade.php
@@ -124,7 +124,7 @@
                             Normal Migrate
                         </button>
                         <button wire:click="migrate(true)" x-on:click.prevent="showMigrateModal = false" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 hover:bg-blue-700 text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm mt-3 sm:mt-0">
-                            Safe Migrate
+                            Safe Re-Migrate all
                         </button>
                     </div>
                     <button x-on:click.prevent="showMigrateModal = false" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm mt-3 sm:mt-0">
