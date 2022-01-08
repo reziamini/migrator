41c78ec105da7fbc5534f4bca66b471c57ddf3d7
adjarriawan <muhammad.rizky.adjarriawan@gmail.com>
2022-01-08 16:41:55 +0800

    Revert "change arrow functions to anonymous functions"
    This reverts commit d8be7fd5c376f27d45d72b0aa957aa7a63bd2291.

--- src/Service/StructureParser.php
+++ src/Service/StructureParser.php
@@ -2,91 +2,95 @@
 
 namespace Migrator\Service;
 
-class StructureParser
-{
+Class StructureParser {
 
-    public $matches;
-    public $structure = array();
+	public $matches;
+	public $structure = array();
 
-    public function __construct($matches)
+	public function __construct($matches)
     {
         $this->matches = $matches;
     }
 
-    public function getStructure()
-    {
-        foreach ($this->matches[0] as $match) {
-            $match = trim(str_replace(';', '', $match));
+	public function getStructure()
+	{
+		foreach($this->matches[0] as $match) {
+			$match = trim(str_replace(';', '', $match));
 
-            $match = substr($match, strpos($match, '->') + 2);
-            $type = $this->getType($match);
+			$match = substr($match, strpos($match, '->') + 2);
+			$type = $this->getType($match);
 
-            $this->structure[] = [
+			$this->structure[] = [
                 'name'     => $this->getName($match, $type),
                 'type'     => $type,
                 'nullable' => $this->checkNullable($match),
                 'unique'   => $this->checkUnique($match),
                 'default'  => $this->checkDefault($match)
             ];
+		}
+
+		$phpVersion = explode('.', phpversion());
+
+        if ($phpVersion[0] > 7 && $phpVersion[1] < 4) {
+            return array_filter($this->structure, function ($value) {
+                return ! is_null($value['type']);
+            });
         }
-
-        $checkType = function ($value) {
-            return ! is_null($value['type']);
-        };
-
-        return array_filter($this->structure, $checkType);
-    }
-
-    private function getType($match)
-    {
-        preg_match("/^(\w+)\(.*\).*/si", $match, $newMatches);
-
-        return $newMatches[1] ?? '';
-    }
-
-    private function getName($match, $type)
-    {
-        $matches = [
-            'id'                 => 'id',
-            'nullableTimestamps' => 'created_at, updated_at',
-            'timestampsTz'       => 'created_at, updated_at',
-            'timestamps'         => 'created_at, updated_at',
-            'rememberToken'      => 'remember_token',
-        ];
-
-        if (in_array($type, array_keys($matches))) {
-            return $matches[$type];
+        else {
+            return array_filter($this->structure, fn($value) => ! is_null($value['type']));
         }
+	}
 
-        if (in_array($type, ['enum', 'set'])) {
-            return substr($match, stripos($match, "'") + 1, (stripos($match, ",") - stripos($match, "'") - 2));
-        }
+	private function checkUnique($match)
+	{
+		return strpos($match, '->unique()');
+	}
 
-        if ($type === 'foreignIdFor') {
-            return substr($match, stripos($match, "(") + 1, (stripos($match, ")") - stripos($match, "(") - 1));
-        }
-
-        return substr($match, stripos($match, "'") + 1, stripos(substr($match, stripos($match, "'") + 1), "'"));
-    }
-
-    private function checkNullable($match)
-    {
-        return strpos($match, '->nullable()');
-    }
-
-    private function checkUnique($match)
-    {
-        return strpos($match, '->unique()');
-    }
-
-    private function checkDefault($match)
-    {
-        if ( ! strpos($match, '->default(')) {
+	private function checkDefault($match)
+	{
+		if (!strpos($match, '->default(')) {
             return false;
         }
 
-        preg_match('/->default\((.*?)\).*/', $match, $newMatch);
+		preg_match('/->default\((.*?)\).*/', $match, $newMatch);
 
         return str_replace(['"', '\''], '', $newMatch[1] ?? '');
-    }
+	}
+
+	private function checkNullable($match)
+	{
+		return strpos($match, '->nullable()');
+	}
+
+	private function getName($match, $type)
+	{
+	    $matches = [
+	        'id' => 'id',
+            'nullableTimestamps' => 'created_at, updated_at',
+            'timestampsTz' => 'created_at, updated_at',
+            'timestamps' => 'created_at, updated_at',
+            'rememberToken' => 'remember_token',
+        ];
+
+	    if (in_array($type, array_keys($matches))){
+	        return $matches[$type];
+        }
+
+	    if (in_array($type, ['enum', 'set'])){
+	        return substr($match, stripos($match, "'") + 1, (stripos($match, ",") - stripos($match, "'") - 2));
+        }
+
+	    if ($type === 'foreignIdFor'){
+	        return substr($match, stripos($match, "(") + 1, (stripos($match, ")") - stripos($match, "(") - 1));
+        }
+
+		return substr($match, stripos($match, "'") + 1, stripos(substr($match, stripos($match, "'") + 1), "'"));
+	}
+
+	private function getType($match)
+	{
+        preg_match("/^(\w+)\(.*\).*/si", $match, $newMatches);
+
+        return $newMatches[1] ?? '';
+	}
 }
