9ef7af1d911cbff6a94771a71dd8e6ccd7196bea
Abbas mkhzomi <llabbasmkhll@gmail.com>
2022-01-08 12:30:12 +0330

    batch added to migrations table

--- resources/views/livewire/migration/read.blade.php
+++ resources/views/livewire/migration/read.blade.php
@@ -18,7 +18,7 @@
                                 Connection
                             </th>
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Ran?
+                                Batch
                             </th>
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Created At
