962c184bd57a7eb22053f0fa2c954c0fb4cc97f3
adjarriawan <muhammad.rizky.adjarriawan@gmail.com>
2022-01-08 15:51:48 +0800

    Add select2 on dropdown select connection and action

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,6 +7,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.2.0/dist/select2-bootstrap-5-theme.min.css"
+          rel="stylesheet" type="text/css">
     @livewireStyles
     <title>Migrator - {{ $title }}</title>
     <script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
@@ -46,6 +49,18 @@
 
     });
 
+</script><script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.full.min.js"></script>
+<script>
+    $('select:eq(0)').select2({
+        theme: 'bootstrap-5',
+        placeholder: 'Select Connection',
+    });
+
+    $('select:eq(1)').select2({
+        theme: 'bootstrap-5',
+        placeholder: 'Select Action',
+    });
 </script>
 </body>
 </html>
