420b7bfec55ea7511e028a858cab2da695753526
Reza Amini <rezaaminiroyal@gmail.com>
2022-01-10 15:06:12 +0330

    Refactor config section

--- config/migrator.php
+++ config/migrator.php
@@ -1,29 +1,35 @@
 <?php
 
 return [
-    // Path to access migrator page
-    'route'      => 'migrator',
+    /*
+    |--------------------------------------------------------------------------
+    | Migrator Route
+    | Migrator route address to access the module: 'localhost:8000/migrator'
+    |--------------------------------------------------------------------------
+    */
+    'route' => 'migrator',
 
-    // Middleware to authorize the admin user
+    /*
+    |--------------------------------------------------------------------------
+    | Middlewares for Migrator
+    | Middlewares which are used to access Migrator route
+    |--------------------------------------------------------------------------
+    */
     'middleware' => 'auth',
 
     /*
     |--------------------------------------------------------------------------
     | Only on local
-    |--------------------------------------------------------------------------
-    |
     | Flag that preventing showing commands if environment is on production
-    |
+    |--------------------------------------------------------------------------
     */
-    'local'      => true,
+    'local' => true,
 
     /*
     |--------------------------------------------------------------------------
     | Paginate show data per page
-    |--------------------------------------------------------------------------
-    |
     | Showing data with default paginate per page 10
-    |
+    |--------------------------------------------------------------------------
     */
-    'per_page'   => 10,
+    'per_page' => 15,
 ];
