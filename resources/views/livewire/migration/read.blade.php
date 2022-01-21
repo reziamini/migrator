127c7ac6ac7547a9c6ceef6401a16218fb83dc89
Steve Beaudry <live.remix@gmail.com>
2022-01-20 19:51:17 -0500

    Update read.blade.php

--- resources/views/livewire/migration/read.blade.php
+++ resources/views/livewire/migration/read.blade.php
@@ -18,7 +18,7 @@
                                 Connection
                             </th>
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Batch
+                                Batch Number
                             </th>
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Created At
