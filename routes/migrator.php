c366a7e4edd463bcfcd603459a079674a77cd740
Reza Amini <rezaaminiroyal@gmail.com>
2022-01-25 22:17:03 +0330

    Add support for multi middlewares

--- routes/migrator.php
+++ routes/migrator.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Migrator\Http\Livewire\Migration\Read;
 
 Route::get(config('migrator.route'), Read::class)
-    ->name('migrator.read')->middleware(config('migrator.middleware') ? ['web', config('migrator.middleware')] : ['web']);
+    ->name('migrator.read')->middleware(
+        !config('migrator.middleware') ? ['web'] : array_unique(array_merge(['web'], (array) config('migrator.middleware')))
+    );
